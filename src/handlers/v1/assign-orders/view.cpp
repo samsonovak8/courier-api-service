cc172f9d7c021a7a95f512a9649807c3eed71baa
Katya Samsonova <2715309@gmail.com>
2024-02-11 13:29:22 +0300

    improved assign-order readability

--- src/handlers/v1/assign-orders/view.cpp
+++ src/handlers/v1/assign-orders/view.cpp
@@ -8,13 +8,13 @@
 #include <userver/storages/postgres/cluster.hpp>
 #include <userver/storages/postgres/component.hpp>
 
-#include "../../../models/order.hpp"
-#include "../../../models/courier.hpp"
-#include <typeinfo>
-#include <vector>
 #include <algorithm>
 #include <set>
+#include <typeinfo>
 #include <unordered_map>
+#include <vector>
+#include "../../../models/courier.hpp"
+#include "../../../models/order.hpp"
 
 namespace DeliveryService {
 
@@ -25,7 +25,7 @@ class AssignOrders final : public userver::server::handlers::HttpHandlerBase {
   static constexpr std::string_view kName = "handler-v1-assign-orders";
 
   AssignOrders(const userver::components::ComponentConfig& config,
-              const userver::components::ComponentContext& component_context)
+               const userver::components::ComponentContext& component_context)
       : HttpHandlerBase(config, component_context),
         pg_cluster_(
             component_context
@@ -35,89 +35,113 @@ class AssignOrders final : public userver::server::handlers::HttpHandlerBase {
   std::string HandleRequestThrow(
       const userver::server::http::HttpRequest& request,
       userver::server::request::RequestContext&) const override {
-      
-    auto uncompleted_orders_set = pg_cluster_->Execute(
-        userver::storages::postgres::ClusterHostType::kMaster,
-        "SELECT * FROM delivery_service.order "
-        "WHERE completed_time = $1",
-        "0");
+    auto uncompleted_orders = GetUncompletedOrders();
+    auto couriers = GetSortedCouriers();
 
-    std::vector<TOrder> uncompleted_orders;
-    userver::formats::json::ValueBuilder response;
-    response["items"].Resize(0);
-    for (auto row : uncompleted_orders_set.AsSetOf<TOrder>(
-             userver::storages::postgres::kRowTag)) {
-      //response["items"].PushBack(row);
-      uncompleted_orders.push_back(row);
-    }
-    std::sort(uncompleted_orders.begin(), uncompleted_orders.end(), [&](TOrder& a, TOrder& b) {
-      int time1 = ((a.delivery_hours[0] - '0') * 10 + (a.delivery_hours[1] - '0')) * 60 + (a.delivery_hours[3] - '0') * 10 + (a.delivery_hours[4] - '0');
-      int time2 = ((b.delivery_hours[0] - '0') * 10 + (b.delivery_hours[1] - '0')) * 60 + (b.delivery_hours[3] - '0') * 10 + (b.delivery_hours[4] - '0');
-      return time1 < time2;
-    });
-    
-    auto couriers_set = pg_cluster_->Execute(
-        userver::storages::postgres::ClusterHostType::kMaster,
-        "SELECT * FROM delivery_service.courier"
-        );
-    
-    auto comp = [](const TCourier& a, const TCourier& b) {
-      int time1 = ((a.working_hours[0] - '0') * 10 + (a.working_hours[1] - '0')) * 60 + (a.working_hours[3] - '0') * 10 + (a.working_hours[4] - '0');
-      int time2 = ((b.working_hours[0] - '0') * 10 + (b.working_hours[1] - '0')) * 60 + (b.working_hours[3] - '0') * 10 + (b.working_hours[4] - '0');
-      return time1 < time2;
-    };
-    std::set<TCourier, decltype(comp)> couriers(comp);
-    for (auto row : couriers_set.AsSetOf<TCourier>(
-            userver::storages::postgres::kRowTag))  {
-        couriers.insert(row);
+    for (const auto& order : uncompleted_orders) {
+      auto assigned_courier = FindCourierForOrder(order, couriers);
+      if (!assigned_courier.empty()) {
+        UpdateOrderWithCourier(order.id, assigned_courier);
       }
-      
-    for (auto order : uncompleted_orders) {
-      bool courier_found = false;
-      std::string assigned_courier = "";
-      for (auto courier : couriers) {
-        int time1 = ((order.delivery_hours[0] - '0') * 10 + (order.delivery_hours[1] - '0')) * 60 + ((order.delivery_hours[3] - '0') * 10 + (order.delivery_hours[4] - '0'));
-        int time2 = ((order.delivery_hours[6] - '0') * 10 + (order.delivery_hours[7] - '0')) * 60 + ((order.delivery_hours[9] - '0') * 10 + (order.delivery_hours[10] - '0'));
-        int time3 = ((courier.working_hours[0] - '0') * 10 + (courier.working_hours[1] - '0')) * 60 + ((courier.working_hours[3] - '0') * 10 + (courier.working_hours[4] - '0'));
-        int add = 0;
-        if (courier.transport == "пеший") {
-          add = 25;
-          if (std::stoi(order.weight) <= 10 && order.region == courier.region && time3 + add >= time1 && time3 + add <= time2) {
-            courier_found = true;
-          }
-        } else if (courier.transport == "велокурьер") {
-          add = 12;
-          if (std::stoi(order.weight) <= 20 && time3 + add >= time1 && time3 + add <= time2) {
-            courier_found = true;
-          }
-        } else {
-          add = 8;
-          if (std::stoi(order.weight) <= 40 && time3 + add >= time1 && time3 + add <= time2) {
-            courier_found = true;
-          }
-        }
-        if (courier_found) {
-          assigned_courier = courier.id;
-          couriers.erase(courier);
-          // courier.working_hours += add;
-          couriers.insert(courier);
-          break;
-        }
-      }
-     
-      auto couriers_set = pg_cluster_->Execute(
-        userver::storages::postgres::ClusterHostType::kMaster,
-        "UPDATE delivery_service.order "
-        "SET courier_id=$1 "
-        "WHERE id=$2",
-        assigned_courier, order.id);
     }
-    // return ???
-    
-    return "userver::formats::json::ToString(response.ExtractValue())";
+
+    return userver::formats::json::ToString(response.ExtractValue());
   }
 
  private:
+  std::vector<TOrder> GetUncompletedOrders() const {
+    auto uncompleted_orders_set = pg_cluster_->Execute(
+        userver::storages::postgres::ClusterHostType::kMaster,
+        "SELECT * FROM delivery_service.order WHERE completed_time = $1", "0");
+
+    return uncompleted_orders_set.AsSetOf<TOrder>(
+        userver::storages::postgres::kRowTag);
+  }
+
+  std::set<TCourier> GetSortedCouriers() const {
+    auto couriers_set = pg_cluster_->Execute(
+        userver::storages::postgres::ClusterHostType::kMaster,
+        "SELECT * FROM delivery_service.courier");
+
+    auto comp = [](const TCourier& a, const TCourier& b) {
+      return ConvertToMinutes(a.working_hours) <
+             ConvertToMinutes(b.working_hours);
+    };
+
+    return std::set<TCourier, decltype(comp)>(
+        couriers_set.AsSetOf<TCourier>(userver::storages::postgres::kRowTag)
+            .begin(),
+        couriers_set.AsSetOf<TCourier>(userver::storages::postgres::kRowTag)
+            .end(),
+        comp);
+  }
+  int ConvertToMinutes(const std::string& time) const {
+    return ((time[0] - '0') * 10 + (time[1] - '0')) * 60 +
+           ((time[3] - '0') * 10 + (time[4] - '0'));
+  }
+  
+  std::string FindCourierForOrder(const TOrder& order,
+                                  std::set<TCourier>& couriers) const {
+    for (auto& courier : couriers) {
+
+      if (IsCourierEligible(order, courier, order.delivery_hours,
+                            courier.working_hours, courier.transport)) {
+        couriers.erase(courier);
+        courier.working_hours += add;
+        couriers.insert(courier);
+        return courier.id;
+      }
+    }
+
+    return "";  // No suitable courier found
+  }
+
+  int GetTimeDelay(const std::string& transport) const {
+    std::unordered_map<std::string, std::string> time_delay;
+    time_delay["пеший"] = 25;
+    time_delay["велокурьер"] = 12;
+    time_delay["авто"] = 8;
+    return time_delay[transport];
+  }
+  bool CanDeliverOnTime(const std::string& order_delivery_hours,
+                        const std::string& courier_working_hours,
+                        const std::string& courier_transport) const {
+    int delivery_delay = GetTimeDelay(courier.transport);
+    int order_interval_begin =
+        ConvertToMinutes(order_delivery_hours.substr(0, 5));
+    int order_interval_end =
+        ConvertToMinutes(order_delivery_hours.substr(6, 11));
+    int courier_free_interval_begin = ConvertToMinutes(courier_working_hours);
+    return (order_interval_begin <=
+            courier_free_interval_begin + delivery_delay) &&
+           (courier_free_interval_begin + delivery_delay <= order_interval_end);
+  }
+
+  bool IsCourierEligible(const TOrder& order, const TCourier& courier,
+                         const std::string& order_delivery_hours,
+                         const std::string& courier_working_hours,
+                         const std::string& courier_transport) const {
+    return std::stoi(order.weight) <= GetMaxWeight(courier.transport) &&
+           order.region == courier.region &&
+           CanDeliverOnTime(order_delivery_hours, courier_working_hours,
+                            delivery_delay);
+  }
+
+  int GetMaxWeight(const std::string& transport) const {
+    std::unordered_map<std::string, std::string> max_weight;
+    max_weight["пеший"] = 10;
+    max_weight["велокурьер"] = 20;
+    max_weight["авто"] = 40;
+    return max_weight[transport];
+  }
+
+  void UpdateOrderWithCourier(const std::string& order_id,
+                              const std::string& courier_id) const {
+    pg_cluster_->Execute(
+        userver::storages::postgres::ClusterHostType::kMaster,
+        "UPDATE delivery_service.order SET courier_id=$1 WHERE id=$2",
+        courier_id, order_id);
+  }
   userver::storages::postgres::ClusterPtr pg_cluster_;
 };
 
